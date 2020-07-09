8b2e345e152eeebb6105c75c4710a4024e3b9158
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-07-09 16:06:02 +0530

    Added the regex patterns and event listeners on key up to input fields to check if the entered information is valid or invalid and styled accordingly.

--- validation.js
+++ validation.js
@@ -1,1 +1,28 @@
 // validation script here
+const inputs = document.querySelectorAll('input');
+
+
+const patterns = {
+    phone: /^\d{10}$/,
+    username: /^\w{5,12}$/i,
+    password: /^[\w@-]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([\w.-]+)@([\w-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/i
+
+};
+
+function validate(field, pattern){
+    // console.log(pattern.test(field.value))
+    if(pattern.test(field.value)){
+        field.className = 'valid';
+    }else{
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup',(e)=>{
+        // console.log(e.target.attributes.name.value)
+        validate(e.target, patterns[e.target.attributes.name.value])
+    })
+})
